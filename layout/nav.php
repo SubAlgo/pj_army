978b8a6a31f5e4526a1ddc4db6620af71ba578f1
korn <kornchaval@gmail.com>
2018-01-14 21:20:54 +0700

    add project_show

--- layout/nav.php
+++ layout/nav.php
@@ -1,6 +1,8 @@
 
 <nav>
   <ul>
+    <li><a href="./index.php">หน้า Login</a></li>
+    <hr>
     <li><a href="./project_show.php">แสดง รายการ/โครงการ</a></li>
     <hr>
     <li><a href="./project_add.php">เพิ่ม รายการ/โครงการ</a></li>
--- project_show.php
+++ project_show.php
@@ -2,6 +2,10 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="./css/mystyle.css">
+<link rel="stylesheet" type="text/css" href="./css/table.css">
+
+
+
 </head>
 <body>
 
@@ -15,7 +19,38 @@
 <!-- +++++++++++++++++ Content +++++++++++++++++ -->
 
 <article>
-  show
+  
+      <label>กรุณากดเลือก รายการ/โครงการ</label>
+
+      <table id="customers">
+         <tr>
+             <th>ลำดับ</th>
+             <th>รายการ/โครงการ</th>
+         </tr>
+
+         <tr>
+             <td align="center">1</td>
+             <td><a href="./project_show.php">จัดซื้อวัสดุสำนักงาน งวดที่ 1 จำนวน 60 รายการ</a></td>
+             
+         </tr>
+
+         <tr>
+             <td align="center">2</td>
+             <td>จ้างปรับปรุงอาคารและสถานที่ จำนวน 2 จุด</td>
+         </tr>
+
+         <tr>
+             <td align="center">3</td>
+             <td>ช่อมรถยนต์ส่วนกลาง กห 0070 จำนวน 2 รายการ</td>
+         </tr>
+
+         <tr>
+             <td align="center">4</td>
+             <td>จัดซื้อคอมพิวเตอร์ (โน็ตบุ๊ค) จำนวน 10 เครื่อง</td>
+         </tr>
+
+      </table>
+  
 </article>
 
 <!-- +++++++++++++++++ Content +++++++++++++++++ -->
